db4a9947fd99d411617f8f4f5830e4078fdfbef8
Maurizio Piroddi <x.hacker.hiphop99@gmail.com>
2025-12-10 00:31:51 +0100

    Inizio esercizio

---
+++ 01_saluti/script.js
@@ -0,0 +1,15 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+let nameOfUser;
+
+
+// Dichiara la funzione qui.
+
+let saluto = () => console.log('Ciao' + nameOfUser);
+
+// Invoca la funzione qui e stampa il risultato in console
+
+(userName);
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
